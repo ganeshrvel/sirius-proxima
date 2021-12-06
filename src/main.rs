8ec797d182036c4c20f146fbe0fe1f8f1be6acde
Ganesh Rathinavel <ganeshrvel@outlook.com>
2021-12-06 11:33:07 +0530

    code cleanup

--- src/main.rs
+++ src/main.rs
@@ -88,38 +88,14 @@ async fn run() -> anyhow::Result<()> {
     // app data
     let app_data = AppData::new().await?;
     let shared_app_data = actix_web::web::Data::new(app_data);
-    let server_url_with_protocol = shared_app_data
-        .config
-        .app_settings
-        .settings
-        .server
-        .get_uri(true)?;
-    let server_url_without_protocol = shared_app_data
-        .config
-        .app_settings
-        .settings
-        .server
-        .get_uri(false)?;
-    let cookie_secret = shared_app_data
-        .config
-        .app_settings
-        .settings
-        .server
-        .cookie_secret
-        .clone();
-    let domain = shared_app_data
-        .config
-        .app_settings
-        .settings
-        .server
-        .get_domain()?;
-    let cookie_max_age_secs = shared_app_data
-        .config
-        .app_settings
-        .settings
-        .server
-        .cookie_max_age_secs;
+
     let server = shared_app_data.config.app_settings.settings.server.clone();
+    let server_url_with_protocol = server.get_uri(true)?;
+    let server_url_without_protocol = server.get_uri(false)?;
+    let cookie_secret = server.cookie_secret.clone();
+    let domain = server.get_domain()?;
+    let cookie_max_age_secs = server.cookie_max_age_secs;
+
     log::info!("starting the server on: {}", server_url_with_protocol);
 
     // app state
